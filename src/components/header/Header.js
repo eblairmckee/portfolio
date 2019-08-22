8e8b0e4a37b559eede5ee53c08ced8e21eec4021
Blair McKee <bmckee@tiggee.com>
2019-08-22 07:09:27 -0400

    minor style changes to header. added descriptions to portfolio.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 export function Header() {
 	return (
 		<header>
-			<h1>Hi, I'm Blair.</h1>
-			<h3>I'm a front end developer & designer</h3>
+			<div className="header__wrapper">
+				<h1>Hi, I'm Blair.</h1>
+				<h3>I'm a front end developer & designer</h3>
+			</div>
 		</header>
 	);
 }

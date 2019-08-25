e1a0bab2e32202b2854bc842828cea0de7fe0d4d
Blair McKee <bmckee@tiggee.com>
2019-08-24 20:34:45 -0400

    darkened dark mode

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import theme from 'styled-theming';
 
 export const backgroundColor = theme('mode', {
 	light: '#fff',
-	dark: '#35123f'
+	dark: '#08001f'
 });
 
 export const textColor = theme('mode', {
@@ -22,7 +22,7 @@ export const buttonTextColor = theme('mode', {
 
 export const headerBackgroundColor = theme('mode', {
 	light: 'linear-gradient( #33fff0, #ffbdd5, #fff)',
-	dark: 'linear-gradient(#b50887, #9b218a, #633073,#35123f)'
+	dark: 'linear-gradient(#08001f, #40004d, #633073,#35123f, #40004d, #08001f)'
 });
 
 export const headingColor = theme('mode', {
@@ -31,6 +31,6 @@ export const headingColor = theme('mode', {
 });
 
 export const headingShadow = theme('mode', {
-	light: '5px 5px #b50887',
-	dark: '5px 5px rgba(255, 255, 255, 0.5)'
+	light: '3px 3px #b50887',
+	dark: '3px 3px rgba(255, 255, 255, 0.5)'
 });
